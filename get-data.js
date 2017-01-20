5c54617c65a947692cdf5a1e39530eeb3a8a17e5
Neelabh Kashyap <Neelabh.Kashyap@bigdatapump.com>
2017-01-20 14:00:08 +0200

    secured connection string

--- get-data.js
+++ get-data.js
@@ -1,6 +1,7 @@
 // Imports
 let WebSocket = require('ws');
 let EventHubClient = require('azure-event-hubs').Client
+let config = require('./config.js')
 
 // Subscribe to BTCUSD pair
 let reqSubscribeTrades = {
@@ -10,7 +11,7 @@ let reqSubscribeTrades = {
 }
 
 // Create Event Hub Client
-let client = EventHubClient.fromConnectionString('Endpoint=sb://btc-trades.servicebus.windows.net/;SharedAccessKeyName=relayTrades;SharedAccessKey=zh6uE0WdbYVJD30mpVpV1y2gaCugvkYNflSF1VipsDM=', 'btc-usd')
+let client = EventHubClient.fromConnectionString(config.connectionString, 'btc-usd')
 senderPromise = client.open().then(() => client.createSender('1'))
 
 // Interact with bitfinex API
@@ -29,7 +30,7 @@ w.onmessage = (msg) => {
                 size: Math.abs(wsMsg[6]),
                 direction: Math.sign(wsMsg[6])
             }
-        } else if (wsMsg[1] == 'te') {
+        } else {
             return trade = {
                 type: wsMsg[1],
                 seq: wsMsg[2],
@@ -38,11 +39,9 @@ w.onmessage = (msg) => {
                 size: Math.abs(wsMsg[5]),
                 direction: Math.sign(wsMsg[5])
             }
-        } else {
-            return trade = { type: "hb" }
         }
     }
-    if (wsMsg instanceof Array && !(wsMsg[1] instanceof Array)) {
+    if (wsMsg instanceof Array && !(wsMsg[1] instanceof Array) && wsMsg[1] != 'hb') {
         console.log(JSON.stringify(getTradeObject()))
     }
 
